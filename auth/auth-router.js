1ce80a2057363ead4dabd8a2f17518a6eb6067c0
Your Name <you@example.com>
2019-09-18 09:30:53 -0700

    got token back

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 
 const Users = require('../users/users-model.js');
+const jwt = require('jsonwebtoken')
 
 // for endpoints beginning with /api/auth
 router.post('/register', (req, res) => {
@@ -25,8 +26,9 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
         res.status(200).json({
-          message: `Welcome ${user.username}!`,
+          message: `Welcome ${user.username}!`,token
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -36,5 +38,16 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function generateToken(user){
+  const payload = {
+    username: user.username,
+  };
+  const secrets = 'keep it secret'
+  const options = {
+    expiresIn: '1d'
+  };
+  return jwt.sign(payload, secrets, options)
+}
 
 module.exports = router;
